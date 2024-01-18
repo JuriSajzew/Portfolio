b141bd4f1d6af53341a036df94221115b0be50e6
Juri Sajzew <j.sajzew@outlook.de>
2024-01-18 17:06:39 +0100

    update img

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './landing-page/header/header.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent }
+    { path: '', component: LandingPageComponent },
 ];
