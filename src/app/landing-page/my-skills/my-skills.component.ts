a718dfc5d3655cd8abb69479db3888240b64aa34
Juri Sajzew <j.sajzew@outlook.de>
2024-01-21 18:43:21 +0100

    create my skills

--- src/app/landing-page/my-skills/my-skills.component.ts
+++ src/app/landing-page/my-skills/my-skills.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+  iconImg: string[] = [
+    'assets/img/shadows/angular.svg',
+    'assets/img/shadows/typescript.svg',
+    'assets/img/shadows/javaScript.svg',
+    'assets/img/shadows/html.svg',
+    'assets/img/shadows/firebase.svg',
+    'assets/img/shadows/git.svg',
+    'assets/img/shadows/css.svg',
+    'assets/img/shadows/rest_api.svg',
+    'assets/img/shadows/scrum.svg',
+    'assets/img/shadows/material_design.svg'
+  ];
 
+  iconText: string[] = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'Firebase',
+    'GIT',
+    'CSS',
+    'Rest-Api',
+    'Scrum',
+    'Material design'
+  ]
 }
