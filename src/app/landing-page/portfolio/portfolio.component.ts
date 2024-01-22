6450eb8ef0833f3255d39eeae86ab0d46746c347
Juri Sajzew <j.sajzew@outlook.de>
2024-01-22 13:06:11 +0100

    creat portfolio

--- src/app/landing-page/portfolio/portfolio.component.ts
+++ src/app/landing-page/portfolio/portfolio.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+  projectImg: string[] = [
+    'assets/img/join.svg',
+    'assets/img/el_pollo_loco.svg'
+  ];
+
+  projectName: string[] = [
+    'Join',
+    'El Pollo Loco'
+  ];
+
+  framework: string[] = [
+    'Angular | Typescript | HTML | CSS | Firebase',
+    'JavaScript | HTML | CSS'
+  ];
+  
+  projectText: string[] = [
+    'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+    'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.'
+  ];
 
 }
